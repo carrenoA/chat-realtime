950b5eb766b38af1cfd80da023832d522369d28a
carrenoA <juan.david.carreno.arias01@gmail.com>
2025-08-10 17:40:49 -0500

    Conexión docker a backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,14 +2,21 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ChatModule } from './modules/chat/chat.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'production' ? '.env' : '.env.local',
     }),
-    MongooseModule.forRoot('mongodb://localhost:27017/chat_db'),
+    MongooseModule.forRoot(
+      process.env.MONGO_URI ?? 'mongodb://localhost:27017/chat_db'
+    ),
     ChatModule,
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
